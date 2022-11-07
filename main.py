7116140ce791f30bdb8c92ce56e2526156576443
Juan Martin Vazquez <juavazquez@frba.utn.edu.ar>
2022-11-07 15:56:38 -0300

    hola mundo

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print('hola')
 print("Hola desde la rama")
-=======
+
 
 def saludar():
     return 3
->>>>>>> ca576351a26fbc82bf8156444c72e1571ec215e4
-=======
+
+
 print('hola chau')
 
 print('hola mundo')
@@ -19,4 +17,7 @@ def saludar():
 
 
 print('pulleado')
->>>>>>> 979ae0396d243226463c5f2b5bba1bbf53d8d6bc
+
+
+def holaMundo():
+    print('hola mundo!')
